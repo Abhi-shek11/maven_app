3029ad670ddc78f99168c693785832555b4c2be5
Abhi-shek11 <komuravelliabhishek1193@gmail.com>
2023-12-06 11:15:02 +0530

    maven java app push

---
+++ src/main/java/Abhishek/JAVA_APP/App.java
@@ -0,0 +1,13 @@
+package Abhishek.JAVA_APP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
